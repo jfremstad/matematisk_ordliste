f9d706ae4b624edf167e56060fab06fd3238efd9
Joakim Fremstad <joa.fre@gmail.com>
2018-10-01 19:59:47 +0200

    Legg til test for tom linje

--- tester.sh
+++ tester.sh
@@ -4,11 +4,11 @@ DATABASE="verifiserte_termer.csv"
 # Check if CSV is valid
 csvlint-v0.2.0-linux-amd64/csvlint "$DATABASE" || exit 1
 
-# Check for empty comments
-if [[ "$(grep -c ",\s\+$" $DATABASE)" -ne 0 ]]; then
-  echo "==============================="
-  echo " Tom merknad i følgende linjer "
-  echo "==============================="
-  grep ",\s\+$" "$DATABASE"
+# Check for empty comments or empty lines
+if [[ "$(grep -Ec ",\s\+$|^\s*$" $DATABASE)" -ne 0 ]]; then
+  echo "======================================================="
+  echo "             Det er feil i følgende linjer             "
+  echo "======================================================="
+  grep -En ",\s\+$|^\s*$" "$DATABASE"
   exit 2
 fi
