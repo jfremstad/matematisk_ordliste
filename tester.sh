b18440a84def3572c885d065e6c6fbdd211fdcb7
Joakim Fremstad <joa.fre@gmail.com>
2018-10-01 20:03:00 +0200

    Fiks test for tom merknad

--- tester.sh
+++ tester.sh
@@ -5,10 +5,10 @@ DATABASE="verifiserte_termer.csv"
 csvlint-v0.2.0-linux-amd64/csvlint "$DATABASE" || exit 1
 
 # Check for empty comments or empty lines
-if [[ "$(grep -Ec ",\s\+$|^\s*$" $DATABASE)" -ne 0 ]]; then
+if [[ "$(grep -Ec ",\s+$|^\s*$" $DATABASE)" -ne 0 ]]; then
   echo "======================================================="
   echo "             Det er feil i følgende linjer             "
   echo "======================================================="
-  grep -En ",\s\+$|^\s*$" "$DATABASE"
+  grep -En ",\s+$|^\s*$" "$DATABASE"
   exit 2
 fi
