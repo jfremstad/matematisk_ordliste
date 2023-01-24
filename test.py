2cd3083b243a6b37280a9dd6a8d2d5211a0041db
Fredrik Bakke <fredrbak@gmail.com>
2023-01-24 15:38:25 +0100

    Forbedringer til automatisk kvalitetskontroll (#151)
    * Kvalitetskontroll flaggsystem
    
    Bytt til flaggsystem, slik at feilkoden viser alle typer feil som er oppdaget dersom flere har inntruffet.
    
    * feilkodekorreksjon
    
    * Fiks overlapp av feilkoder
    
    * navnekonvensjon
    
    * fjern ubrukt modul
    
    * forenkle checkSorted
    
    * legg til rad og kolonne i feilmelding
    flytt systeminteraksjon ut fra checkSorted
    
    * bytt til 2-roms indentering i python
    
    * bytt til standardiseringsbasert sjekk
    
    * bruk str.casefold
    
    * sjekk duplikate rader men ignorer kommentar
    
    * Fjern overflødige mellomrom
    
    * Fjern duplikate rader uten kommentar
    
    * sjekk for CRLF
    
    * sjekk for <br></br> o.l.
    
    * gjentatt sortering
    
    * numberOfLanguageColumns konstant
    
    * forenklede betingelsesuttrykk

--- test.py
+++ test.py
@@ -3,40 +3,78 @@
 
 import csv
 import sys
-import re
 
+lineBreak = "<br>"
+numberOfLanguageColumns = 3
 
-# Check if list is sorted
-def isSorted(a):
-    return all(a[i] <= a[i+1] for i in range(len(a)-1))
+# Fjerner gjentatte mellomrom og trimmer endene
+trim = lambda s: ' '.join(s.split())
 
 # Returns a list of lists. Each list is one row.
-def readcsv(filename):
-    data = []
-    with open(filename, encoding='utf-8', errors='ignore') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
-        for row in reader:
-            data.append(list(map(lambda x:x.lower(), row)))
-    return list(filter(None, data)) # Remove empty lists
+def readCsv(filename):
+  data = []
+  with open(filename, encoding='utf-8', errors='ignore') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for row in reader:
+      data.append(list(map(str.casefold, row)))
+  return list(filter(None, data)) # Remove empty rows
 
-def checkSorted(data):
-    EXITCODE = 0
-    errorlist = []
-    for row in data:
-        for col in range(3):
-            synonyms = row[col].split("<br>")
-            if not isSorted(row[col].split("<br>")):
-                errorlist.append(row[col])
-                EXITCODE = 5
-    if EXITCODE == 5:
-        print("Endringer som må gjøres:\n")
-        for error in errorlist:
-            print(error, "-->", "<br>".join(sorted(error.split("<br>"))))
-    sys.exit(EXITCODE)
+# Standardiserer en celle
+def standardizeCell(cell):
+  # Fjern alternative linjebytter
+  cell = cell.replace("\r\n", lineBreak).replace("\n", lineBreak).replace("<br/>", lineBreak).replace("<br />", lineBreak).replace("<br></br>", lineBreak)
+  # Del opp
+  synonyms = cell.split(lineBreak)
+  # Trim mellomrom
+  synonyms = map(trim, synonyms)
+  # Fjern tomme synonymer
+  synonyms = filter(lambda x: x != "", synonyms)
+  # Fjern duplikater
+  synonyms = set(synonyms)
+  # Sorter
+  synonyms = sorted(synonyms)
+  # Sett sammen
+  return lineBreak.join(synonyms)
+
+# Sjekker om alle celler er standardiserte
+def checkStandardized(data):
+  errors = []
+  for r, row in enumerate(data):
+    for c in range(numberOfLanguageColumns):
+      std = standardizeCell(row[c])
+      if std != row[c]:
+        errors.append((r, c, row[c], std))
+  return errors
+
+# Sjekk for duplikate rader ved ignorering av kommentar
+def checkDuplicateRows(data):
+  rows = tuple(",".join(row[:numberOfLanguageColumns]) for row in data)
+  duplicates = set(row for row in rows if rows.count(row) > 1)
+  return duplicates
 
 
 def main():
-    data = readcsv(sys.argv[1])
-    checkSorted(data)
+  exitCode = 0
+  data = readCsv(sys.argv[1])
+
+  nonstandard = checkStandardized(data)
+
+  if nonstandard:
+    exitCode |= 32
+    print("\nFølgende endringer må gjøres (merk mulig fjerning av mellomrom):\n")
+    for r, c, error, correct in nonstandard:
+      print("Rad", r + 1, "kolonne", c + 1 , ":", error, "-->", correct)
+    print()
+
+  duplicateRows = checkDuplicateRows(data)
+  
+  if duplicateRows:
+    exitCode |= 64
+    print("\nFølgende rader er duplikater opp til kommentar:\n")
+    for row in duplicateRows:
+      print(row)
+
+  sys.exit(exitCode)
+
 
 main()
