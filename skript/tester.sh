1e372168e1688ff7c6791a6c5f46e042809927ff
Fredrik Bakke <fredrbak@gmail.com>
2023-10-04 17:16:51 +0200

    forbedre robusthet og lesbarhet i `tester.sh` - Legger til konstanter for feilkoder for å forbedre lesbarheten og vedlikeholdbarheten. - Inkluderer en funksjon for å sjekke om nødvendige verktøy som csvlint og Python3 er installert. - Legger til en sjekk for å se om databasefilen eksisterer før skriptet kjøres. - Qvoterer variabler for å gjøre skriptet mer robust mot spesielle tegn i strenger.

--- skript/tester.sh
+++ skript/tester.sh
@@ -1,50 +1,88 @@
 #!/bin/bash
+
 DATABASE="./verifiserte_termer.csv"
+
+ERR_DB_IKKE_FUNNET=1
+ERR_CSVLINT_IKKE_FUNNET=2
+ERR_PYTHON_IKKE_FUNNET=4
+ERR_TOM_MERKNAD=8
+ERR_TOM_LINJE=16
+ERR_DUPLIKAT=32
+ERR_MANGLENDE_ANFORSELSTEGN=64
+
 EXITCODE=0
 
-# Check if CSV is valid
-csvlint-v0.2.0-linux-amd64/csvlint "${DATABASE}" || EXITCODE=$((EXITCODE | 1))
+# Function to check if a command exists
+command_exists () {
+  command -v "$1" >/dev/null 2>&1
+}
 
-EMPTYCOMMENT="$(grep ',\s\+$' ${DATABASE})"
+# Check if database exists
+if [[ ! -f "${DATABASE}" ]]; then
+  echo "Feil: Databasefilen '${DATABASE}' ikke funnet."
+  EXITCODE=$((EXITCODE | ERR_DB_IKKE_FUNNET))
+  exit ${EXITCODE}
+fi
+
+
+# Check if CSV is valid
+if command_exists csvlint; then
+  csvlint "${DATABASE}" || EXITCODE=$((EXITCODE | ERR_CSVLINT_IKKE_FUNNET))
+else
+  echo "Feil: csvlint ikke funnet."
+  EXITCODE=$((EXITCODE | ERR_CSVLINT_IKKE_FUNNET))
+fi
+
+EMPTYCOMMENT="$(grep ',\s\+$' "${DATABASE}")"
 if [[ ${EMPTYCOMMENT} ]]; then
   echo "====================================="
   echo "== Følgende linjer har tom merknad =="
   echo "====================================="
   echo "${EMPTYCOMMENT}"
   echo
-  EXITCODE=$((EXITCODE | 2))
+  EXITCODE=$((EXITCODE | ERR_TOM_MERKNAD))
 fi
 
-if [[ "$(grep -Ec "^\s*$" ${DATABASE})" -ne 0 ]]; then
+if [[ "$(grep -Ec "^\s*$" "${DATABASE}")" -ne 0 ]]; then
   echo "=============================="
   echo "== Følgende linjer er tomme =="
   echo "=============================="
   grep -n '^\s*$' "${DATABASE}"
   echo
-  EXITCODE=$((EXITCODE | 4))
+  EXITCODE=$((EXITCODE | ERR_TOM_LINJE))
 fi
 
-DUPLICATES="$(sort ${DATABASE} | uniq -d)"
+DUPLICATES="$(sort "${DATABASE}" | uniq -d)"
 if [[ ${DUPLICATES} ]]; then
   echo "============================================="
   echo "== Ordlista inneholder følgende duplikater =="
   echo "============================================="
   echo "${DUPLICATES}"
   echo
-  EXITCODE=$((EXITCODE | 8))
+  EXITCODE=$((EXITCODE | ERR_DUPLIKAT))
 fi
 
-MISSINGQUOTES="$(grep -v ',\s\+$' ${DATABASE} | cut -d',' -f 4- | grep -vE '^$|^".*"$')"
+MISSINGQUOTES="$(grep -v ',\s\+$' "${DATABASE}" | cut -d',' -f 4- | grep -vE '^$|^".*"$')"
 if [[ ${MISSINGQUOTES} ]]; then
   echo "=============================================="
   echo "== Følgende merknader mangler anførselstegn =="
   echo "=============================================="
   echo "${MISSINGQUOTES}"
   echo
-  EXITCODE=$((EXITCODE | 16))
+  EXITCODE=$((EXITCODE | ERR_MANGLENDE_ANFORSELSTEGN))
 fi
 
-# Execute more complicated python tests
-python3 ./skript/test.py "${DATABASE}"
+# Execute more complicated Python tests
+if command_exists python3; then
+  python3 ./skript/test.py "${DATABASE}"
+else
+  echo "Feil: Python3 ikke funnet."
+  EXITCODE=$((EXITCODE | ERR_PYTHON_IKKE_FUNNET))
+fi
+
+# Check if EXITCODE is 0
+if [[ ${EXITCODE} -eq 0 ]]; then
+  echo "Alle sjekker ble gjennomført uten feil."
+fi
 
 exit ${EXITCODE}
